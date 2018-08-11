191f3691f1a03b5f1a43c3f153571ae03456dd43
mc-zone <mc@mc-zone.me>
2018-08-11 17:14:49 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-git subtree push --prefix dist origin gh-pages
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+cd ./dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f origin master:gh-pages
+cd -
